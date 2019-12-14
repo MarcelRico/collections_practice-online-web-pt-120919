04ad4ff7c0968a3dcfc0123f413c6328cdc32cf5
Marcel Rico <personal.mr4@gmail.com>
2019-12-14 16:50:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort{|a,b| b<=>a}
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  swap_elements_from_to(array,1,2)
+end
+
+def swap_elements_from_to(array,index,destination_index)
+  temp = array[index]
+  array[index] = array[destination_index]
+  array[destination_index] = temp
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  result = []
+  array.each do |word|
+    
+  end
+end
