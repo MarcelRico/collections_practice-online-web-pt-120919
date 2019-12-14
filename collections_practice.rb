404841460bc378ae27fbfef72e39749b5ec0ca8a
Marcel Rico <personal.mr4@gmail.com>
2019-12-14 17:07:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,5 +45,9 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element,index|}
+  array.each_with_index.collect do |element,index|
+    if element != 2
+      array << "s"
+    end
+  end
 end
